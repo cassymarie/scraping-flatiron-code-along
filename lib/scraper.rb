bf071787a74fd11c9ffe25506110b486c7434000
cassymarie <cassy2205@gmail.com>
2020-11-05 08:17:49 -0700

    commit tree 19d81192f56c29dc50d96d5bfc35b29d6988e49b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,37 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
+
+  #reposnsible for using Nokogiri and `open-uri` to grab HTML doc
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) 
+    
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+    
+    # binding.pry
+  end 
+
+  #use CSS selector to grab HTML elements
+  def get_courses
+
+  end
+
+  #instantiating Course objects given the 3 attributes
+  def make_courses(title, schedule, description)
+
+
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +42,7 @@ class Scraper
       end
     end
   end
-  
+
 end
 
-
-
+Scraper.new.get_page
